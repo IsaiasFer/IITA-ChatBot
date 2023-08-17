470bdec9251d9b78fb60b2b7eab4cf2caa4ad29c
Isaias Fernandez <isafernande04@gmail.com>
2023-08-17 14:20:31 -0300

    Multicanal listo

--- chatgpt.class.js
+++ chatgpt.class.js
@@ -1,7 +1,18 @@
-require('dotenv').config()
+require("dotenv").config();
+const { join } = require("path");
+const { readFileSync } = require("fs");
 
+const getPrompt = async () => {
+  const pathPromp = join(process.cwd(), "prompts");
+  const text = readFileSync(join(pathPromp, "prompt.txt"), "utf-8");
+  return text;
+};
+
+// Variables creadas por Isaias
+let numbersDiccionary = {};
+let conversaciones = [];
 class ChatGPTClass {
-  queue = []; 
+  queue = [];
   optionsGPT = { model: "gpt-4" };
   openai = undefined;
 
@@ -14,30 +25,54 @@ class ChatGPTClass {
    */
   init = async () => {
     const { ChatGPTAPI } = await import("chatgpt");
-    this.openai = new ChatGPTAPI(
-        {
-          apiKey: process.env.OPENAI_API_KEY
-        }
-    );
+    this.openai = new ChatGPTAPI({
+      apiKey: process.env.OPENAI_API_KEY,
+      systemMessage: await getPrompt(),
+      n: 1,
+    });
   };
 
-  /**
+  /* *
    * Manejador de los mensajes
    * sun funcion es enviar un mensaje a wahtsapp
-   * @param {*} ctx 
+   * @param {*} ctx
    */
-  handleMsgChatGPT = async (body) => {
+
+  // Este codigo se ejecuta cada vez que se recibe un mensaje a IITA
+  handleMsgChatGPT = async (body, ctx) => {
+    // Numero de telefono de la persona que manda el mensaje
+    let numeroDelCliente = ctx.from;
+
+    // Comprueba si ya existe un historial con el cliente. SI no existe, crea un array vacío para empezarlo
+    if (!conversaciones[numeroDelCliente]) {
+      conversaciones[numeroDelCliente] = []
+    }
+
+    // Solo sirve para entender mejor el código
+    let mensajesDelCliente=conversaciones[numeroDelCliente]
+
+
+    // Comprueba si el número que envió mensaje ya está registrado en el diccionario de números
+    if (!Object.keys(numbersDiccionary).includes(`${numeroDelCliente}`)) {
+      console.log("Este numero no estaba registrado, lo registrare")
+      numbersDiccionary[numeroDelCliente] =
+        Object.keys(numbersDiccionary).length;
+    }
+
+    // Le proprociona contexto al mensaje de ChatGPT, mediante su ID de conversación y el mensaje anterior o ensaje "Padre" este código sirve para que el ChatBot sea multicanal y mantenga información diferenciada por cada chat.
     const interaccionChatGPT = await this.openai.sendMessage(body, {
-      conversationId: !this.queue.length
-        ? undefined 
-        : this.queue[this.queue.length - 1].conversationId,
-      parentMessageId: !this.queue.length
+      conversationId: numbersDiccionary[numeroDelCliente],
+      parentMessageId: mensajesDelCliente.length==0
         ? undefined
-        : this.queue[this.queue.length - 1].id,
+        : mensajesDelCliente[mensajesDelCliente.length - 1].id,
     });
 
-    this.queue.push(interaccionChatGPT);
-    return interaccionChatGPT
+    // Este código agrega el último mensaje que mandó el chatbot a la cadena de mensajes historicos
+    mensajesDelCliente.push(interaccionChatGPT);
+
+/*     console.log("esta es la lista de mensaje que se enviaron: ");
+    console.log(mensajesDelCliente); */
+    return interaccionChatGPT;
   };
 }
 
--- flows/flowVenta.js
+++ flows/flowVenta.js
@@ -1,56 +1,14 @@
 const { addKeyword } = require("@bot-whatsapp/bot");
 const revision = require("../revisionDeChat.js");
 /* const { getUser, getTicket } = require("../api/users.service"); */
-const { readFileSync } = require("fs");
-const { join } = require("path");
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 const isa = require("../modules/funcionesImportantes.js");
-
-/* // Modulos para la Bases de datos externas
-// Import the functions you need from the SDKs you need
-const { initializeApp } = require("firebase/app");
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// COnfiguracion de la conexion con Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyAEkrWjltWRM9R6mGqS13HWdNKFmTJcYPo",
-  authDomain: "chatbot-iita.firebaseapp.com",
-  databaseURL: "https://chatbot-iita-default-rtdb.firebaseio.com",
-  projectId: "chatbot-iita",
-  storageBucket: "chatbot-iita.appspot.com",
-  messagingSenderId: "673913631253",
-  appId: "1:673913631253:web:fc9492778e94da7e6f941c",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-const {
-  getDatabase,
-  ref,
-  set,
-  get,
-  child,
-  update,
-  remove,
-} = require("firebase/database");
-const db = getDatabase(); */
 
 // modulos para usar logs internos dea
 /* var require$$4 = require("console");
 var require$$5 = require("fs");
 const { Console } = require$$4;
 const { createWriteStream } = require$$5; */
-
-/**
- * Recuperamos el prompt "TECNICO"
- */
-const getPrompt = async () => {
-  const pathPromp = join(process.cwd(), "prompts");
-  const text = readFileSync(join(pathPromp, "prompt.txt"), "utf-8");
-  return text;
-};
 
 /**
  * Exportamos
@@ -61,55 +19,11 @@ module.exports = {
   flowVenta: (chatgptClass) => {
     return addKeyword("1", {
       sensitive: true,
-      /* regex:true */
+      /* regex:true  <-- Para expresiones Regulares*/
     })
       .addAction(async (ctx, { endFlow, flowDynamic, provider }) => {
-        await flowDynamic("El siguiente mensaje será hecho por ChatGPT");
-
-        /*         const jid = ctx.key.remoteJid;
-        const refProvider = await provider.getInstance();
-
-        await refProvider.presenceSubscribe(jid);
-        await delay(500);
-
-        await refProvider.sendPresenceUpdate("composing", jid); */
-
-        /*         const user = await getUser(ctx.from); //Consultamos a strapi! ctx.from = numero
-
-        const lastTicket = await getTicket(user[0].id);
-
-        if (!lastTicket.data.length) {
-          await flowDynamic("No tienes ticket abierto!");
-          return endFlow();
-        }
-
-        const listTickets = lastTicket.data
-          .map(
-            (i) =>
-              `ID_REF:${i.id}, cliente:${user[0].username}, model:${i.attributes.model}, description: ${i.attributes.description}, status:${i.attributes.status}`
-          )
-          .join("\n"); */
-
-        const data = await getPrompt();
-
-        await chatgptClass.handleMsgChatGPT(data); //Dicinedole actua!!
-
-        const textFromAI = await chatgptClass.handleMsgChatGPT(`${ctx.body}`);
-
-        /* Aqui se registra en un txt el mensaje que va a enviar ChatGPT */
-        /* logger = new Console({
-          stdout: createWriteStream(`${process.cwd()}/chats/${ctx.nombre}.json`, {
-            flags: "a",
-          }),
-        });
-        logger.log(
-          "{'ChatGPT envió el mensaje",
-          textFromAI.text,
-          "el día de hoy'}"
-        ); */
-        /* console.log(revision(textFromAI.text)); */
+        const textFromAI = await chatgptClass.handleMsgChatGPT(ctx.body,ctx);
         await flowDynamic(textFromAI.text);
-        /* insertarMensaje(ctx.nombre, textFromAI.text, ctx.fecha, ctx.timestamp); */
         isa.insertarMensaje(
           ctx.nombre,
           textFromAI.text,
@@ -126,7 +40,7 @@ module.exports = {
           // ctx.body = es lo que la persona escribe!!
 
           if (!ctx.body.toLowerCase().includes("ofertas")) {
-            const textFromAI = await chatgptClass.handleMsgChatGPT(ctx.body);
+            const textFromAI = await chatgptClass.handleMsgChatGPT(ctx.body,ctx);
             isa.insertarMensaje(
               ctx.nombre,
               textFromAI.text,
